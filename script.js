b6048ce8563ad7b2f9973ae5eead40c066c1b62e
feder205 <31936361+feder205@users.noreply.github.com>
2023-09-09 20:45:17 -0300

    Nuevo archivo CV

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToSection(sectionId) {
+    const section = document.querySelector(`.${sectionId}`);
+    if (section) {
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
